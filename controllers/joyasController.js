91d0a1879bd7e672af457acce51e6d936bff8b5a
merakissb <mbustosquiroz@gmail.com>
2023-10-15 14:29:46 -0300

    adds logic into getJoyasFiltros get method

--- controllers/joyasController.js
+++ controllers/joyasController.js
@@ -33,9 +33,30 @@ export const getJoyas = async (req, res) => {
     }
 
     const dbInstance = await pool.connect();
+
     const response = await dbInstance.query(query, params);
 
-    res.status(200).json(response.rows);
+    const stockTotalResult = await dbInstance.query(`
+      SELECT SUM(stock) AS total FROM inventario
+      WHERE id IN (${response.rows.map(joya => joya.id).join(',')})
+    `);
+
+    const stockTotal = parseInt(stockTotalResult.rows[0].total, 10) || 0;
+
+    if (isNaN(stockTotal)) {
+      stockTotal = 0;
+    }
+
+    res.status(200).json({
+      totalJoyas: response.rows.length,
+      stockTotal: parseInt(stockTotal, ),
+      results: response.rows.map((joya) => ({
+        id: joya.id,
+        nombre: joya.nombre,
+        precio: joya.precio,
+        stock: joya.stock,
+      })),
+    });
     dbInstance.release();
   } catch (error) {
     console.log(error);
@@ -44,4 +65,47 @@ export const getJoyas = async (req, res) => {
 };
 
 export const getJoyasFiltros = async (req, res) => {
+  try {
+    const precio_min = parseInt(req.query.precio_min, 10);
+    const precio_max = parseInt(req.query.precio_max, 10);
+    const categoria = req.query.categoria;
+    const metal = req.query.metal;
+
+    if (isNaN(precio_min) || isNaN(precio_max) || precio_min > precio_max) {
+      return res.status(400).json({ error: 'Parámetros de precio inválidos' });
+    }
+
+    let query = 'SELECT * FROM inventario'
+    const params = [];
+
+    if (precio_min) {
+      query += ' WHERE precio >= $1';
+      params.push(precio_min);
+    }
+
+    if (precio_max) {
+      query += ' AND precio <= $2';
+      params.push(precio_max);
+    }
+
+    if (categoria) {
+      query += ' AND categoria = $3';
+      params.push(categoria);
+    }
+
+    if (metal) {
+      query += ' AND metal = $4';
+      params.push(metal);
+    }
+
+    const dbInstance = await pool.connect();
+
+    const response = await dbInstance.query(query, params);
+
+    res.status(200).json(response.rows);
+    dbInstance.release();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
 };
