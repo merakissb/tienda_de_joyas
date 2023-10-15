a94718584d6a221c5811cc5fd098fbc13543c1f6
merakissb <mbustosquiroz@gmail.com>
2023-10-15 13:42:03 -0300

    feat: adds params in get /joyas route

--- controllers/joyasController.js
+++ controllers/joyasController.js
@@ -2,19 +2,40 @@
 
 import { pool } from '../db/pool.js';
 
-// debe poder recibir limit, page, order_by a traves de req.query
-// ej: http://localhost:3001/joyas?limits=3&page=2&order_by=stock_ASC
 export const getJoyas = async (req, res) => {
   try {
-    const dbInstance = await pool.connect();
-    const { limits, page, order_by } = req.query;
+
+    const limits = parseInt(req.query.limits, 10);
+    const page = parseInt(req.query.page, 10);
+    const order_by = req.query.order_by;
+
+    if (limits < 0 || page < 1) {
+      return res.status(400).json({ error: 'invalid params' });
+    }
 
     let query = 'SELECT * FROM inventario';
-    let params = [];
+    const params = [];
 
+    if (limits) {
+      query += ' LIMIT $1';
+      params.push(limits);
+    }
+
+    if (page) {
+      query += ' OFFSET $2';
+      params.push((page - 1) * limits);
+    }
+
+    if (order_by) {
+      const order = order_by.split('_');
+      query += ' ORDER BY $3 $4';
+      params.push(order[0], order[1]);
+    }
+
+    const dbInstance = await pool.connect();
     const response = await dbInstance.query(query, params);
-    res.status(200).json(response.rows);
 
+    res.status(200).json(response.rows);
     dbInstance.release();
   } catch (error) {
     console.log(error);
